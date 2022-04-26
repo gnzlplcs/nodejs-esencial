704d3b4db50d377a29b8408679645470e4e1e095
gnzlplcs <palaciosgm@outlook.com>
2022-04-26 13:56:16 -0500

    first file: app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// using moment package
+// const moment = require('moment');
+// const birthday = "1983-02-21";
+// console.log(moment(birthday).format('DD/MM/YY').toString())
+
+const miModulo = require('./miModulo');
+
+console.log(miModulo.sumar(21, 21))
